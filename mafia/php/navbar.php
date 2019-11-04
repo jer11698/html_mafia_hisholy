58648fe9ae7938773175a37bb388db93ea828831
jer11698 <jer11698.553@outlook.com>
2019-11-04 17:40:31 +0700

    เพิ่ม php จัดการแยก html กับ php

---
+++ mafia/php/navbar.php
@@ -0,0 +1,11 @@
+<nav>
+  <label for="drop" class="toggle" align="center">Menu</label>
+  <input type="checkbox" id="drop" />
+  <ul class="menu">
+    <li><a href="#">ผู้จัดทำ</a></li>
+    <li><a href="#">ข่าวสาร</a></li>
+    <li><a href="#">กลุ่มมาเฟีย</a></li>
+    <li><a href="#">ประวัติ</a></li>
+    <li><a href="#">หน้าแรก</a></li>
+  </ul>
+</nav>
